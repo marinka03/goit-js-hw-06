934f3c1b76b0f8ad6d794fef1bf2487061ef3bfb
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-02 09:56:27 +0300

    add-first-task

---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+const categories = document.querySelector("#categories");
+
+const liArr = [...categories.children];
+
+const liCount = `Number of categories: ${liArr.length}`
+console.dir(liCount);
+
+const result = liArr.forEach((item) => {
+const ul = [...item.lastElementChild.children];
+console.log(`Category: ${item.firstElementChild.textContent}
+Elements: ${ul.length}`);
+});
