511a890e17385282f7a231ed3965e65ef855ee82
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-02 16:09:07 +0300

    update-second-task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ul = document.querySelector("#ingredients");
+const li = ingredients.map(ingredient => { 
+  const el = document.createElement('li');
+  el.textContent = ingredient;
+  el.classList.add("item");
+  return el;
+});
+
+ul.append(...li);
