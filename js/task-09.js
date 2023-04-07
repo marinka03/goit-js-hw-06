f17d2606d6ac9567cc6af18996b683641742e843
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-07 22:56:35 +0300

    add-task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+button.addEventListener("click", onClick);
+
+function onClick(evt){
+  const color = getRandomHexColor()
+body.style.backgroundColor = color;
+span.textContent = color;
+}
