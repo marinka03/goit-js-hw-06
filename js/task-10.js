ee9eff18abd53868d47a83f67515cb1daadec1a5
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-08 03:08:30 +0300

    add-last-task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("controls");
+const boxes = document.querySelector("#boxes");
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const input = document.querySelector("input");
+
+createBtn.addEventListener("click", () => createBoxes(input.value));
+destroyBtn.addEventListener("click", destroyBoxes);
+boxes.classList.add('box-style');
+
+function createEl (size){
+const element = document.createElement("div");
+element.style.width = size + "px";
+element.style.height = size + "px";
+element.style.backgroundColor = getRandomHexColor();
+
+boxes.append(element);
+};
+
+function createBoxes(amount){
+let size;
+for(let i = 0; i < amount; i++){
+  size = 30 + 10 * i;
+  createEl(size);
+};
+};
+
+function destroyBoxes(){
+  boxes.replaceChildren();
+};
+
+
